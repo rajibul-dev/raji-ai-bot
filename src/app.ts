72a8cdb8399aa9cfdcf3c2622e4ac469fb12fba2
Rajibul Islam <rajibulski110@gmail.com>
2025-03-28 16:21:38 +0530

    inital commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+function log(message: string) {
+  console.log(message);
+}
+
+function sayHello(name: string) {
+  console.log("Hello " + name + "!");
+}
+
+log("hello Node + Typescript");
+sayHello("Rajibul");
